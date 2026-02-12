93a6b99555cd9e09528048d47e3303a284e4691f
Matheus V <matheusvini754@gmail.com>
2026-02-12 00:59:59 -0300

    adicionado na repository findyByStatus

--- src/main/java/com/miranda/todo_API/Repository/TaskRepository.java
+++ src/main/java/com/miranda/todo_API/Repository/TaskRepository.java
@@ -1,9 +1,14 @@
 package com.miranda.todo_API.Repository;
 
 import com.miranda.todo_API.Entity.TaskEntity;
+import com.miranda.todo_API.model.TaskStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TaskRepository extends JpaRepository <TaskEntity, Long> {
+
+    List<TaskEntity>findbyStatus(TaskStatus status);
 }
